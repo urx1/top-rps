7efb9a03464af88be5668f3f3a39430e153c7514
urx1 <urkkinen@proton.me>
2023-01-19 23:15:59 +0200

    added computer choice

--- rps.js
+++ rps.js
@@ -1,4 +1,19 @@
 //get user choice and store in variable//
 
-let user = prompt("rock (r), paper (p) or scissors (s)");
+
+let user_choice = prompt("rock (r), paper (p) or scissors (s)");
+    user_choice = user_choice.toLowerCase();
+
+function computer_choice() {
+    const computer_choices = ["r", "p", "s"];
+    const random = Math.floor(Math.random() * 3);
+    console.log (computer_choices[random])
+}
+
+console.log(user_choice);
+computer_choice();
+
+
+//console.log(user_choice);//
+//console.log(computer_choice, choices[computer_choice]);//
 
