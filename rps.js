2935abe28e31e49180e45895ddbbe5deedc464ad
urx1 <urkkinen@proton.me>
2023-01-24 23:25:35 +0200

    Completed game functionality
    Completed game functionality. Added script to reset computer and user choice after each round to allow replaying.

--- rps.js
+++ rps.js
@@ -2,18 +2,22 @@
 
 //needs to be made into a loop that starts with click on button//
 
-let user_choice=""
+var user_choice=""
 
 function user_rock() {
     user_choice += "r";
+    game();
 }
 
 function user_paper() {
     user_choice += "p";
+    game(); 
 }
 function user_scissors() {
     user_choice += "s";
+    game()
 }
+
 
 
 
@@ -25,33 +29,41 @@ let user_choice = prompt("r p or s?")
 */
 
 function game() {
-    let computer_choices = ["r", "p", "s"];
-    let random = Math.floor(Math.random() * 3);
+    var computer_choices = ["r", "p", "s"];
+    var random = Math.floor(Math.random() * 3);
     //console.log(user_choice);//
     //console.log(computer_choices[random])//
     if  (user_choice === computer_choices[random]) { 
-        console.log("tie"); }
+        console.log("tie");
+        user_choice="";
+    }
     else if (user_choice === "r" && computer_choices[random] === "p") {
         console.log("computer chose paper. you lose");
+        user_choice="";
     } else if (user_choice ==="r" && computer_choices[random] === "s") {
         console.log("computer chose scissors. you win.");
+        user_choice="";
     } else if (user_choice === "p" && computer_choices[random] === "r") {
         console.log("computer chose rock. you win");
+        user_choice="";
     } else if (user_choice === "p" && computer_choices[random] === "s") {
         console.log("computer chose scissors. you lose");
+        user_choice="";
     } else if (user_choice === "s" && computer_choices[random] === "r") {
         console.log("computer chose rock. you lose");
+        user_choice="";
     } else if (user_choice === "s" && computer_choices[random] === "p") {
         console.log("computer chose paper. you win");
+        user_choice="";
     }
       else {
         console.log("not a tie");
+        user_choice="";
     }
 }
 
 //game()//
 
-console.log(user_choice)
 
 
 //console.log(user_choice);//
