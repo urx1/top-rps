4ef4328ca7cd32b099b9987c66a51b2623d43873
urx1 <urkkinen@proton.me>
2023-01-26 14:27:17 +0200

    Display game result
    Made game result display in HTML. Still have to add functional points counter.

--- rps.js
+++ rps.js
@@ -2,7 +2,10 @@
 
 //needs to be made into a loop that starts with click on button//
 
-var user_choice=""
+var user_choice= ""
+
+var p_score = 0
+var c_score = 0
 
 function user_rock() {
     user_choice += "r";
@@ -18,54 +21,57 @@ function user_scissors() {
     game()
 }
 
-
-
-
-/*get user choice and store in variable
-
-let user_choice = prompt("r p or s?")
-    user_choice = user_choice.toLowerCase();
-
-*/
-
 function game() {
     var computer_choices = ["r", "p", "s"];
     var random = Math.floor(Math.random() * 3);
     //console.log(user_choice);//
     //console.log(computer_choices[random])//
     if  (user_choice === computer_choices[random]) { 
-        console.log("tie");
+        document.getElementById("result").innerHTML = "tie";
+        p_score += 0;
+        c_score += 0;
         user_choice="";
     }
     else if (user_choice === "r" && computer_choices[random] === "p") {
-        console.log("computer chose paper. you lose");
+        document.getElementById("result").innerHTML = "computer chose paper. you lose";
+        p_score += 0;
+        c_score += 1;
         user_choice="";
-    } else if (user_choice ==="r" && computer_choices[random] === "s") {
-        console.log("computer chose scissors. you win.");
+    } 
+    else if (user_choice ==="r" && computer_choices[random] === "s") {
+        document.getElementById("result").innerHTML = "computer chose scissors. you win.";
+        p_score += 1;
+        c_score += 0;
         user_choice="";
-    } else if (user_choice === "p" && computer_choices[random] === "r") {
-        console.log("computer chose rock. you win");
+    } 
+    else if (user_choice === "p" && computer_choices[random] === "r") {
+        document.getElementById("result").innerHTML = "computer chose rock. you win.";
+        p_score += 1;
+        c_score += 0;
         user_choice="";
-    } else if (user_choice === "p" && computer_choices[random] === "s") {
-        console.log("computer chose scissors. you lose");
+    } 
+    else if (user_choice === "p" && computer_choices[random] === "s") {
+        document.getElementById("result").innerHTML = "computer chose scissors. you lose.";
+        p_score += 0;
+        c_score += 1;
         user_choice="";
-    } else if (user_choice === "s" && computer_choices[random] === "r") {
-        console.log("computer chose rock. you lose");
+    } 
+    else if (user_choice === "s" && computer_choices[random] === "r") {
+        document.getElementById("result").innerHTML = "computer chose rock. you lose.";
+        p_score += 0;
+        c_score += 1;
         user_choice="";
-    } else if (user_choice === "s" && computer_choices[random] === "p") {
-        console.log("computer chose paper. you win");
+    } 
+    else if (user_choice === "s" && computer_choices[random] === "p") {
+        document.getElementById("result").innerHTML = "computer chose paper. you win.";
+        p_score += 1;
+        c_score += 0;
         user_choice="";
     }
       else {
-        console.log("not a tie");
+        document.getElementById("result").innerHTML = "error";
+
         user_choice="";
     }
 }
-
-//game()//
-
-
-
-//console.log(user_choice);//
-//console.log(computer_choice, choices[computer_choice]);//
 
