2db86d6b2887bebba61db76ea406df550c063158
urx1 <urkkinen@proton.me>
2023-01-26 16:42:38 +0200

    Added score
    Added code to save score of user and computer and print it to page. Bug from earlier was that script was called before html id elements, moving script to bottom of html fixed the problem.

--- rps.js
+++ rps.js
@@ -3,9 +3,8 @@
 //needs to be made into a loop that starts with click on button//
 
 var user_choice= ""
-
-var p_score = 0
-var c_score = 0
+var p_score=0
+var c_score=0
 
 function user_rock() {
     user_choice += "r";
@@ -24,48 +23,54 @@ function user_scissors() {
 function game() {
     var computer_choices = ["r", "p", "s"];
     var random = Math.floor(Math.random() * 3);
-    //console.log(user_choice);//
-    //console.log(computer_choices[random])//
+
     if  (user_choice === computer_choices[random]) { 
         document.getElementById("result").innerHTML = "tie";
         p_score += 0;
         c_score += 0;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     }
     else if (user_choice === "r" && computer_choices[random] === "p") {
         document.getElementById("result").innerHTML = "computer chose paper. you lose";
         p_score += 0;
         c_score += 1;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     } 
     else if (user_choice ==="r" && computer_choices[random] === "s") {
         document.getElementById("result").innerHTML = "computer chose scissors. you win.";
         p_score += 1;
         c_score += 0;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     } 
     else if (user_choice === "p" && computer_choices[random] === "r") {
         document.getElementById("result").innerHTML = "computer chose rock. you win.";
         p_score += 1;
         c_score += 0;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     } 
     else if (user_choice === "p" && computer_choices[random] === "s") {
         document.getElementById("result").innerHTML = "computer chose scissors. you lose.";
         p_score += 0;
         c_score += 1;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     } 
     else if (user_choice === "s" && computer_choices[random] === "r") {
         document.getElementById("result").innerHTML = "computer chose rock. you lose.";
         p_score += 0;
         c_score += 1;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     } 
     else if (user_choice === "s" && computer_choices[random] === "p") {
         document.getElementById("result").innerHTML = "computer chose paper. you win.";
         p_score += 1;
         c_score += 0;
+        document.getElementById("score").innerHTML = `your score: ${p_score}, computer score: ${c_score}`;
         user_choice="";
     }
       else {
